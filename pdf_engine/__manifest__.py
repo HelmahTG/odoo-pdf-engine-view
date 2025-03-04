41b1a409b67568e1709dea48807d275114561026
Helmah <hajar.torabi70@gmail.com>
2025-03-04 11:24:18 +0330

    changed manifest file

--- pdf_engine/__manifest__.py
+++ pdf_engine/__manifest__.py
@@ -2,14 +2,15 @@
     "name": "PDF Engine for Odoo",
     "summary": "A custom PDF Engine for handling large drawing files in Odoo",
     "description": "handeling large drawing files in odoo",
-        This module integrates a custom PDF Viewer into Odoo, allowing users to upload 
+        This module integrates a custom PDF Engine into Odoo, allowing users to upload 
         and review large PDF drawings efficiently. Optimized for architecture and 
         engineering firms working with large blueprint PDFs.
+    """,
     "author": "Helmah",
     "website": "https://rayka-soft.com/",
     "category": "Tools",
     "version": "1.0",
-    "license": "LGPL-3 ",
+    "license": "LGPL-3",
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
